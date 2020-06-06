fdc7f137723b7910fc41c6334c00c4ebc06bcc77
Nathan Roach <nroach2@jhu.edu>
2020-06-06 17:38:41 -0400

    Adding novel loci FASTA extractor

--- conduitUtils.nim
+++ conduitUtils.nim
@@ -1183,8 +1183,8 @@ proc getNovelLociFASTA*(infilepath,gffcompare_infilepath,outfilepath : string,fi
       if fields0[3] != "u" and fields0[3] != "p" and fields0[3] != "y" and fields0[3] != "i" and fields0[3] != "x" and fields0[3] != "s":
         continue
       echo fields0[1]
-      # if fields0[2+field] != "-":
-        novel_loci.incl(getTxId(fields0[2+field]))
+      if fields0[3+field] != "-":
+        novel_loci.incl(getTxId(fields0[3+field]))
         # for tx_id in fields0[2+field].split(','):
           # novel_loci.incl(tx_id)
   except EOFError:
